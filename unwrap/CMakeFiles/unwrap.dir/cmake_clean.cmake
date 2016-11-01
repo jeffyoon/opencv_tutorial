a54b07f9dbf47715883fe46ee959667ae5db7a7e
Jeff Yoon <jeyoon@outlook.com>
2016-11-02 01:25:58 +0900

    Add unwrap code

---
+++ unwrap/CMakeFiles/unwrap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unwrap.dir/unwrap.cpp.o"
+  "unwrap.pdb"
+  "unwrap"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unwrap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
